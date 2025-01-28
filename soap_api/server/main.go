8004ba6fdb71a87e6e6c17c317f3d91df00e53c7
w.sornanankul <m.watcharis@gmail.com>
2025-01-28 16:52:37 +0700

    add zaplog to soap server

--- soap_api/server/main.go
+++ soap_api/server/main.go
@@ -7,7 +7,10 @@ import (
 	"log"
 	"net/http"
 	"watcharis/go-poc-protocal/pkg"
+	"watcharis/go-poc-protocal/pkg/logger"
 	"watcharis/go-poc-protocal/soap_api/models"
+
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -29,31 +32,32 @@ func main() {
 }
 
 func VerifyUserHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 	// Check if the request is POST
 	if r.Method != http.MethodPost {
+		logger.Error(ctx, "method not allow, want http method POST", zap.String("request_method", r.Method))
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
 
-	fmt.Println("methods :", r.Method)
-
 	// Read the request body
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
+		logger.Error(ctx, "Unable to read request body", zap.Error(err))
 		http.Error(w, "Unable to read request body", http.StatusInternalServerError)
 		return
 	}
 	defer r.Body.Close()
 
 	// Log the received request
-	fmt.Printf("Received SOAP Request: %+v\n", string(body))
+	logger.Info(ctx, "Received SOAP Request", zap.String("request_body", string(body)))
 
 	var result models.RequestVerifyUser
 	if err := xml.Unmarshal(body, &result); err != nil {
 		http.Error(w, "Unable to xml.Unmarshal body", http.StatusInternalServerError)
 		return
 	}
-	fmt.Printf("result request_body : %+v\n", result.Body.VerifyUserRequest)
+	logger.Info(ctx, "result xml.Unmarshal request_body", zap.Any("request", result.Body.VerifyUserRequest))
 
 	// Send a mock SOAP response
 	responseTemplate := `<?xml version="1.0" encoding="UTF-8"?>
