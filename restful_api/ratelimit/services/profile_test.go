278e529210eca9e0ea5ae461aa4a8bf89ea5c0a5
w.sornanankul <m.watcharis@gmail.com>
2025-03-25 14:15:18 +0700

    add unittest create profile

---
+++ restful_api/ratelimit/services/profile_test.go
@@ -0,0 +1,93 @@
+package services
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+	"watcharis/go-poc-protocal/pkg/response"
+	"watcharis/go-poc-protocal/restful_api/ratelimit/models"
+	mockRedis "watcharis/go-poc-protocal/restful_api/ratelimit/repositories/cache/mocks"
+	mockDB "watcharis/go-poc-protocal/restful_api/ratelimit/repositories/db/mocks"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_services_CreateUserProfile(t *testing.T) {
+
+	type fields struct {
+		redis              *mockRedis.MockRedisRepository
+		profilesRepository *mockDB.MockProfilesRepository
+		otpRepository      *mockDB.MockOtpRepository
+	}
+
+	type args struct {
+		ctx context.Context
+		req models.ProifleRequest
+	}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		senario func(args args, fields fields)
+		want    models.ProifleResponse
+	}{
+		// TODO: Add test cases.
+		{
+			name: "create user profile success",
+			fields: fields{
+				redis:              mockRedis.NewMockRedisRepository(ctrl),
+				profilesRepository: mockDB.NewMockProfilesRepository(ctrl),
+				otpRepository:      mockDB.NewMockOtpRepository(ctrl),
+			},
+			args: args{
+				ctx: context.Background(),
+				req: models.ProifleRequest{
+					FirstName: "watcharis",
+					LastName:  "sukcha",
+					Email:     "xxx@test.com",
+					Phone:     "0812345678",
+				},
+			},
+			senario: func(args args, fields fields) {
+
+				fields.profilesRepository.EXPECT().CreateUserProfile(args.ctx, gomock.Any()).Return(models.ProfileDB{
+					ID:        1,
+					UUID:      "1234",
+					FirstName: args.req.FirstName,
+					LastName:  args.req.LastName,
+					Email:     args.req.Email,
+					Phone:     args.req.Phone,
+					CreatedAt: time.Date(2021, 10, 10, 10, 10, 10, 10, time.UTC),
+					UpdatedAt: time.Date(2021, 10, 10, 10, 10, 10, 10, time.UTC),
+				}, nil)
+
+				redisProflekey := fmt.Sprintf(models.REDIS_USER_PROFILE, "1234")
+				fields.redis.EXPECT().Hset(args.ctx, redisProflekey, gomock.Any()).Return(int64(1), nil)
+
+			},
+			want: models.ProifleResponse{
+				CommonResponse: response.SetCommonResponse(response.STATUS_SUCCESS, 200),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			tt.senario(tt.args, tt.fields)
+
+			s := NewServices(tt.fields.redis, tt.fields.profilesRepository, tt.fields.otpRepository)
+
+			got, err := s.CreateUserProfile(tt.args.ctx, tt.args.req)
+
+			assert.Equal(t, tt.want, got)
+			assert.NoError(t, err)
+
+		})
+	}
+}
