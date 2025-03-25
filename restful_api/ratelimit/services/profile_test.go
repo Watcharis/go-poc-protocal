c40c31ac649f0a4cde9da11ad56d9a1cd87437b4
w.sornanankul <m.watcharis@gmail.com>
2025-03-25 15:14:17 +0700

    add unittest get profile

--- restful_api/ratelimit/services/profile_test.go
+++ restful_api/ratelimit/services/profile_test.go
@@ -11,6 +11,7 @@ import (
 	mockDB "watcharis/go-poc-protocal/restful_api/ratelimit/repositories/db/mocks"
 
 	"github.com/golang/mock/gomock"
+	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -88,6 +89,115 @@ func Test_services_CreateUserProfile(t *testing.T) {
 			assert.Equal(t, tt.want, got)
 			assert.NoError(t, err)
 
+		})
+	}
+}
+
+func Test_services_GetUserProfile(t *testing.T) {
+	type fields struct {
+		redis              *mockRedis.MockRedisRepository
+		profilesRepository *mockDB.MockProfilesRepository
+		otpRepository      *mockDB.MockOtpRepository
+	}
+	type args struct {
+		ctx  context.Context
+		uuid string
+	}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		senario func(args args, fields fields)
+		want    models.ProifleResponse
+	}{
+		// TODO: Add test cases.
+		{
+			name: "get user profile success",
+			fields: fields{
+				redis:              mockRedis.NewMockRedisRepository(ctrl),
+				profilesRepository: mockDB.NewMockProfilesRepository(ctrl),
+				otpRepository:      mockDB.NewMockOtpRepository(ctrl),
+			},
+			args: args{
+				ctx:  context.Background(),
+				uuid: "1234",
+			},
+			senario: func(args args, fields fields) {
+
+				redisProflekey := fmt.Sprintf(models.REDIS_USER_PROFILE, "1234")
+				fields.redis.EXPECT().HgetallProfile(args.ctx, redisProflekey).Return(models.ProfileDB{
+					ID:        1,
+					UUID:      "1234",
+					FirstName: "watcharis",
+					LastName:  "sukcha",
+					Email:     "xxx@test.com",
+					Phone:     "0812345678",
+				}, nil)
+			},
+			want: models.ProifleResponse{
+				CommonResponse: response.SetCommonResponse(response.STATUS_SUCCESS, 200),
+				Data: &models.ProfileDB{
+					ID:        1,
+					UUID:      "1234",
+					FirstName: "watcharis",
+					LastName:  "sukcha",
+					Email:     "xxx@test.com",
+					Phone:     "0812345678",
+				},
+			},
+		},
+		{
+			name: "get user profile from db success",
+			fields: fields{
+				redis:              mockRedis.NewMockRedisRepository(ctrl),
+				profilesRepository: mockDB.NewMockProfilesRepository(ctrl),
+				otpRepository:      mockDB.NewMockOtpRepository(ctrl),
+			},
+			args: args{
+				ctx:  context.Background(),
+				uuid: "1234",
+			},
+			senario: func(args args, fields fields) {
+
+				redisProflekey := fmt.Sprintf(models.REDIS_USER_PROFILE, "1234")
+				fields.redis.EXPECT().HgetallProfile(args.ctx, redisProflekey).Return(models.ProfileDB{}, redis.Nil)
+
+				fields.profilesRepository.EXPECT().GetUserProfile(args.ctx, args.uuid).Return(models.ProfileDB{
+					ID:        1,
+					UUID:      "1234",
+					FirstName: "watcharis",
+					LastName:  "sukcha",
+					Email:     "xxx@test.com",
+					Phone:     "0812345678",
+				}, nil)
+			},
+			want: models.ProifleResponse{
+				CommonResponse: response.SetCommonResponse(response.STATUS_SUCCESS, 200),
+				Data: &models.ProfileDB{
+					ID:        1,
+					UUID:      "1234",
+					FirstName: "watcharis",
+					LastName:  "sukcha",
+					Email:     "xxx@test.com",
+					Phone:     "0812345678",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.senario(tt.args, tt.fields)
+
+			s := NewServices(tt.fields.redis, tt.fields.profilesRepository, tt.fields.otpRepository)
+
+			got, err := s.GetUserProfile(tt.args.ctx, tt.args.uuid)
+
+			assert.Equal(t, tt.want, got)
+			assert.NoError(t, err)
 		})
 	}
 }
